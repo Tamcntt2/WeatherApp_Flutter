6307b9c29c4acb21886bc7565bf10111892a047f
tamnh2 <tamnh@mirabo-global.com>
2023-05-04 10:18:36 +0700

    add settings

---
+++ lib/screens/home/radar_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RadarPage extends StatelessWidget {
+  const RadarPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
