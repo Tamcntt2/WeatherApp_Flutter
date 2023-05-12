ea80439b760c45cd2d2efec5c88cb82621366c9d
tamnh2 <tamnh@mirabo-global.com>
2023-05-12 11:52:14 +0700

    add radar: custom marker and temp

--- lib/screens/home/radar_page.dart
+++ lib/screens/home/radar_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:weather_app/bloc/weather_bloc.dart';
+import 'package:weather_app/bloc/weather_state.dart';
+import 'package:weather_app/resources/demo_data.dart';
+
 
 class RadarPage extends StatefulWidget {
   const RadarPage({Key? key}) : super(key: key);
@@ -13,13 +18,33 @@ class _RadarPageState extends State<RadarPage> {
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    controller.showMarkerInfoWindow(MarkerId("Demo"));
   }
 
   @override
   Widget build(BuildContext context) {
-    return GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition:
-            CameraPosition(target: LatLng(105.847130, 21.030653), zoom: 12));
+    return BlocBuilder<WeatherBloc, WeatherState>(
+        builder: (context, state) {
+          return GoogleMap(
+            onMapCreated: _onMapCreated,
+            markers: {
+              Marker(
+                markerId: MarkerId("Demo"),
+                position: LatLng(data["lat"] as double, data["lon"] as double),
+                visible: true,
+                infoWindow: InfoWindow(
+                  title: "Ha Noi",
+                  snippet: "${state.forecast!.current!.temp.toString()} °C",
+
+                ),
+              )
+            },
+            initialCameraPosition: CameraPosition(
+              target: LatLng(data["lat"] as double, data["lon"] as double),
+              zoom: 12,
+            ),
+          );
+        }
+    );
   }
 }
