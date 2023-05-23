1a66f9192e59df7155fe4509a362c5743a7046d4
tamnh2 <tamnh@mirabo-global.com>
2023-05-23 16:35:01 +0700

    fix permission location

--- lib/utils/current_location.dart
+++ lib/utils/current_location.dart
@@ -7,14 +7,14 @@ class CurrentLocation {
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      return Future.error('Vị trí bị tắt.');
+      return Future.error('Location is off.');
     }
 
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Không được cấp quyền truy cập vị trí.');
+        return Future.error('Location access is not allowed.');
       }
     }
 
